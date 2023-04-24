c65019ccfc2a070e95cb770c072f20b0cc2b7930
Venky0003 <rajv17270@gmail.com>
2023-04-24 17:36:39 +0530

    add delete method

--- block-BNaacv/user-diary/routes/users.js
+++ block-BNaacv/user-diary/routes/users.js
@@ -1,6 +1,16 @@
 let express = require('express');
 let router = express.Router();
 
+let users = [
+  { name: 'user1', email: 'user1@xyz.com', age: 23, id: 11123 },
+  { name: 'user2', email: 'user2@xyz.com', age: 27, id: 11127 },
+  { name: 'user3', email: 'user3@xyz.com', age: 25, id: 11125 },
+];
+
+router.get('/', (req, res) => {
+  res.render('list', { users });
+});
+
 router.get('/new', (req, res) => {
   res.render('userForm');
 });
@@ -9,34 +19,23 @@ router.post('/', (req, res) => {
   res.json(req.body);
 });
 
-router.post('/', (req, res) => {
-  res.json(req.body);
+router.put('/:id/edit', (req, res) => {
+  let id = req.params.id;
+
+  let user = users.find(user => user.id === Number(id));
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.age = req.body.age;
+
+  res.redirect('/users');
 });
 
-router.get('/:id', (req, res) => {
-  res.render('singleUser', {
-    user: { name: 'Raj', email: 'raj@gmail.com', age: 27 },
-  });
+router.delete('/:id', (req, res) => {
+  let id = req.params.id;
 
-  router.put('/:id', (req, res) => {
-    let id = req.params.id;
-
-    users.forEach((user) => {
-      if (user.id === Number(id)) {
-        user.name = req.body.name;
-        user.email = req.body.email;
-        user.age = req.body.age;
-      }
-    });
-    res.render('/users');
-  });
-
-  router.delete('/:id', (req, res) => {
-    let id = req.params.id;
-
-    users = users.filter((user) => user.id !== Number(id));
-    res.render('/users');
-  });
+  users = users.filter((user) => user.id !== Number(id));
+  res.redirect('/users');
 });
 
 module.exports = router;
