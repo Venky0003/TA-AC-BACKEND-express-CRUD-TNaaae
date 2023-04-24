fa9f42ff809aecb28b19814a097f339404934fdc
Venky0003 <rajv17270@gmail.com>
2023-04-24 10:39:01 +0530

    assignment complete

--- block-BNaacu/student/routes/students.js
+++ block-BNaacu/student/routes/students.js
@@ -5,45 +5,25 @@ let router = express.Router();
 
 //create student form
 router.get('/new', (req, res) => {
-  res.send('Students Form');
+  res.render('students');
   // res.render('forms.ejs')
 });
 
 //data
 router.post('/', (req, res) => {
-  //grab the data
-  //save data to database
-  // send a response
+  res.send(req.body);
 });
 
 //  fetch all students list
 router.get('/', (req, res) => {
   //list all students
-  res.render('students', { list: ['ankit', 'suraj', 'prashant', 'ravi'] });
+  res.render('list', { list: ['ankit', 'suraj', 'prashant', 'ravi'] });
 });
 
-//  single book details
-router.get('/student', (req, res) => {
-  //list a singleform by id
-  //send response with form data
-  res.render('studentDetail', {
-    student: { name: 'rahul', email: 'rahul@altcampus.io' },
-  });
-});
-
-//update the specific book
-router.get('/:id/edit', (req, res) => {
-  //find the book by database
-  //render a update form data
-});
-
-//  uodate it with new data from form
-router.put('/:id', (req, res) => {
-  //findByIdAndUpadate
-});
-
-router.delete('/:id ', (req, res) => {
-  //delete using id
-});
+router.get('/:id', (req, res) => {
+  res.render("studentDetail", {
+    student: { name: "Rahul", email: "rahul@altcampus.io"}
+  })
+})
 
 module.exports = router;
