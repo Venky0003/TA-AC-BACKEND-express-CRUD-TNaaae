ca16fb9e5fcf888e429196e2b9b28f97967c7a34
Venky0003 <rajv17270@gmail.com>
2023-04-26 17:58:44 +0530

    CRUD without using postman

---
+++ block-BNaadj/user-diary/models/user.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email:{ type: String, required: true },
+    age: Number,
+    address: String,
+    bio: String,
+    hobbies: String,
+  },
+  { timestamps: true }
+);
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
